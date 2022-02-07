1766cff97678ea2a205b766231d7920a4c0b98e2
Baptiste Domange <baptiste.domange@gmail.com>
2022-02-07 22:53:15 +0100

    simple player animation

--- Scenes/Character/Character.gd
+++ Scenes/Character/Character.gd
@@ -1,11 +1,28 @@
 extends KinematicBody2D
 
-export var SPEED := 300.0
+onready var animated_sprite = get_node("AnimatedSprite")
+
+export var speed := 300.0
 var direction = Vector2.ZERO
 
 func _process(delta: float) -> void:
-	move_and_slide(direction * SPEED)
+	move_and_slide(direction * speed)
 
 func _input(event: InputEvent) -> void:
-	direction.x = int(event.is_action_pressed('ui_right')) - int(event.is_action_pressed('ui_left'))
-	direction.y = int(event.is_action_pressed('ui_down')) - int(event.is_action_pressed('ui_up'))
+	# direction compute
+	direction.x = int(Input.is_action_pressed('ui_right')) - int(Input.is_action_pressed('ui_left'))
+	direction.y = int(Input.is_action_pressed('ui_down')) - int(Input.is_action_pressed('ui_up'))
+	
+	# slow down diagonal movement
+	direction = direction.normalized()
+	
+	# animation setter
+	match(direction):
+		Vector2.ZERO:
+			animated_sprite.stop()
+			animated_sprite.set_frame(0)
+		Vector2.RIGHT: animated_sprite.play('MoveRight')
+		Vector2.LEFT: animated_sprite.play('MoveLeft')
+		Vector2.DOWN: animated_sprite.play('MoveDown')
+		Vector2.UP: animated_sprite.play('MoveUp')
+		
