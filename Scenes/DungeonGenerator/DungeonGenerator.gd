0a9538841ced2f585a9a87437bf1913350357e3c
Baptiste Domange <baptiste.domange@gmail.com>
2022-07-31 23:18:24 +0200

    dijkstra & sub-walkers

--- Scenes/DungeonGenerator/DungeonGenerator.gd
+++ Scenes/DungeonGenerator/DungeonGenerator.gd
@@ -1,14 +1,36 @@
 extends Node2D
 class_name DungeonGenerator
 
+const MIN_DUNGEON_DEPTH = 7
+
 enum CELL_TYPE {
 	EMPTY, # = 0
 	WALL # = 1
 }
 
-export var grid_size := Vector2(7, 7)
+onready var tilemap = $TileMap
+onready var cell_distances = $CellDistances
+
+export var grid_size := Vector2(10, 10)
+
 var grid : Array = []
-var walker_array : Array = []
+
+var walker_array = []
+var dijkstra_map = []
+
+
+var entry_cell : Vector2
+var exit_cell : Vector2
+
+
+class CellDistance:
+	var cell := Vector2.INF
+	var dist : int = -1
+
+	func _init(_cell: Vector2, _dist: int) -> void:
+		cell = _cell
+		dist = _dist
+
 
 
 #### ACCESSORS ####
@@ -30,16 +52,28 @@ func _ready() -> void:
 #### LOGIC ####
 func _generate_dungeon() -> void:
 	print("Generation started")
-
 	_init_grid()
-	_place_walker()
+	dijkstra_map = []
 
-	while(!walker_array.empty()):
-		for walker in walker_array:
-			var accessible_cells = _get_accessible_cells(walker.cell)
-			walker.step(accessible_cells)
+	while(get_dungeon_depth() < MIN_DUNGEON_DEPTH):
+		_init_grid()
+		entry_cell = _get_random_cell()
+		_place_walker(entry_cell)
+		dijkstra_map = []
 
-	_update_grid_display()
+		while(!walker_array.empty()):
+			for walker in walker_array:
+				var accessible_cells = _get_accessible_cells(walker.cell)
+				walker.step(accessible_cells)
+
+		_update_grid_display()
+		compute_cell_distances(entry_cell)
+
+	var furtherest_cells = get_furtherest_cells()
+	exit_cell = furtherest_cells[randi() % furtherest_cells.size()]
+
+	_place_entry_and_exit_cells()
+	_display_dijkstra_map()
 
 	print("Generation finished")
 
@@ -63,21 +97,24 @@ func _update_grid_display() -> void:
 		for j in range(grid_size.y):
 			var cell_type = grid[i][j]
 
-			$TileMap.set_cell(i, j, cell_type - 1)
+			tilemap.set_cell(i, j, cell_type - 1)
 
 
-func _place_walker() -> void:
-	var ramdom_cell = Vector2(
-		randi() % int(grid_size.x),
-		randi() % int(grid_size.y)
-	)
-
-	var walker = Walker.new(ramdom_cell, 9)
+func _place_walker(cell: Vector2, max_nb_steps: int = 9, nb_sub_walker: int = 2) -> void:
+	var walker = Walker.new(cell, max_nb_steps, nb_sub_walker)
 	walker_array.append(walker)
 	add_child(walker)
 
 	var __ = walker.connect("moved", self, "_on_walker_moved")
 	__ = walker.connect("arrived", self, "_on_walker_arrived", [walker])
+	__ = walker.connect("sub_walker_creation", self, "_on_walker_sub_walker_creation")
+
+
+func _get_random_cell() -> Vector2:
+	return  Vector2(
+		randi() % int(grid_size.x),
+		randi() % int(grid_size.y)
+	)
 
 
 func _get_accessible_cells(cell: Vector2) -> Array:
@@ -95,6 +132,81 @@ func is_inside_grid(cell: Vector2) -> bool:
 	return cell.x >= 0 && cell.x < grid_size.x && \
 					cell.y >= 0 && cell.y < grid_size.y
 
+
+func compute_cell_distances(cell: Vector2, distance : int = 0) -> void:
+	if dijkstra_map.empty():
+		dijkstra_map.append(CellDistance.new(cell, distance))
+
+	distance += 1
+
+	var adjacents = Utils.get_adjacents_cells(cell)
+
+	for adj in adjacents:
+		var tile_id = tilemap.get_cell(adj.x, adj.y)
+
+		if tile_id != -1 or !is_inside_grid(adj):
+			continue
+
+		var cell_dist = get_dijkstra_cell_dist(adj)
+
+		if cell_dist == null:
+			dijkstra_map.append(CellDistance.new(adj, distance))
+			compute_cell_distances(adj, distance)
+
+		elif cell_dist.dist > distance:
+			cell_dist.dist = distance
+			compute_cell_distances(adj, distance)
+
+
+func get_dijkstra_cell_dist(cell: Vector2) -> CellDistance:
+	for cell_dist in dijkstra_map:
+		if cell_dist.cell.is_equal_approx(cell):
+			return cell_dist
+	return null
+
+
+func _display_dijkstra_map() -> void:
+	for child in cell_distances.get_children():
+		child.queue_free()
+
+	var cell_size = tilemap.get_cell_size()
+
+	for cell_dist in dijkstra_map:
+		var label = Label.new()
+		label.text = String(cell_dist.dist)
+
+		label.set_position(cell_dist.cell * cell_size * tilemap.scale)
+		cell_distances.add_child(label)
+
+
+func _place_entry_and_exit_cells() -> void:
+	var entry_id = tilemap.tile_set.find_tile_by_name("Entry")
+	tilemap.set_cellv(entry_cell, entry_id)
+
+	var exit_id = tilemap.tile_set.find_tile_by_name("Exit")
+	tilemap.set_cellv(exit_cell, exit_id)
+
+
+func get_furtherest_cells() -> PoolVector2Array:
+	var dungeon_depth = get_dungeon_depth()
+
+	var furtherest_cells = PoolVector2Array()
+
+	for cell_dist in dijkstra_map:
+		if cell_dist.dist == dungeon_depth:
+			furtherest_cells.append(cell_dist.cell)
+
+	return furtherest_cells
+
+
+func get_dungeon_depth() -> int:
+	var max_depth = 0
+	for cell_dist in dijkstra_map:
+		if cell_dist.dist > max_depth:
+			max_depth = cell_dist.dist
+	return max_depth
+
+
 ### INPUTS ###
 
 func _input(_event: InputEvent) -> void:
@@ -111,3 +223,6 @@ func _on_walker_moved(cell: Vector2) -> void:
 func _on_walker_arrived(walker: Walker) -> void:
 	walker_array.erase(walker)
 	walker.queue_free()
+
+func _on_walker_sub_walker_creation(cell: Vector2, max_nb_steps: int, nb_sub_walker: int) -> void:
+	_place_walker(cell, max_nb_steps, nb_sub_walker)
--- Scenes/DungeonGenerator/Walker.gd
+++ Scenes/DungeonGenerator/Walker.gd
@@ -3,10 +3,14 @@ class_name Walker
 
 var max_steps : int = 0
 var nb_steps : int = 0
+
 var cell = Vector2.INF setget set_cell
+
+var sub_walkers_steps := Array()
 
 signal moved(cell)
 signal arrived
+signal sub_walker_creation
 
 #### ACCESSORS ####
 func is_class(value: String): return value == "Walker" or .is_class(value)
@@ -18,9 +22,10 @@ func set_cell(value: Vector2) -> void:
 		emit_signal("moved", cell)
 
 #### BUILT-IN ####
-func _init(_cell: Vector2, _max_steps: int) -> void:
+func _init(_cell: Vector2, _max_steps: int, nb_sub_walkers: int) -> void:
 	cell = _cell
 	max_steps = _max_steps
+	choose_subwalkers_steps(nb_sub_walkers)
 
 func _ready() -> void:
 	pass
@@ -43,6 +48,10 @@ func step(accesible_cells: Array) -> void:
 
 	set_cell(new_cell)
 
+	if nb_steps in sub_walkers_steps:
+		sub_walkers_steps.erase(nb_steps)
+		emit_signal("sub_walker_creation", cell, 4, 0)
+
 	if nb_steps >= max_steps:
 		emit_signal("arrived")
 
@@ -50,6 +59,17 @@ func step(accesible_cells: Array) -> void:
 func choose_cell(accesible_cells: Array) -> Vector2:
 	var rdm_id = randi() % accesible_cells.size()
 	return accesible_cells[rdm_id]
+
+
+func choose_subwalkers_steps(nb_walkers: int) -> void:
+	var steps_array = range(max_steps)
+	steps_array.shuffle()
+
+	for _i in range(nb_walkers):
+		if steps_array.empty():
+			break
+		var step = steps_array.pop_front()
+		sub_walkers_steps.append(step)
 
 ### INPUTS ###
 
